4e7b6d6b51f5f50a86030966f81f0eec62e68683
Esther van der Laan <esthervdl5@gmail.com>
2024-11-12 11:23:55 +0100

    Update wim.py

--- wim.py
+++ wim.py
@@ -65,8 +65,20 @@ if authentication_status:
     utl.navbar_authenticated(name)
     nav = (st.query_params.get("nav"))
     if nav == "invullen":
-        qoute = random.choice(list_bad_qoutes)
-        st.header(qoute)
+        #qoutes
+        qoute_random = random.choice(list_bad_qoutes)
+        qoute, auteur = qoute_random.split("–")
+        placeholder = st.empty()
+        placeholder1 = st.empty()
+        # Loop through each letter in the text
+        for i in range(len(qoute) + 1):
+            placeholder.header(qoute[:i])  # Update the container with the current substring
+            time.sleep(0.03)     
+        #st.header(qoute)
+        for i in range(len(auteur) + 1):
+            placeholder1.text(auteur[:i])  # Update the container with the current substring
+            time.sleep(0.03)   
+            
         default_start = today -timedelta(days=today.weekday())  # Monday
         default_end = default_start - timedelta(days=4)  # Friday
         refresh_value = timedelta(days=7)
@@ -127,8 +139,8 @@ if authentication_status:
             "jan-maarten",
             "jasper",
             "jeroen",
-            "joostD",
-            "joostH",
+            "joostd",
+            "joosth",
             "kizje",
             "leendert",
             "martijn",
